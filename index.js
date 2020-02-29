0a2794984a7cbdb2bcea8ef34b8e4170eae9a26d
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-29 15:14:45 +0100

    add student router

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const appDebug = require('debug')('app:debug');
 const index_router = require('./routers/index_router');
+const student_router = require('./routers/students');
 const connectDB = require('./db/connect');
 const port = process.env.PORT || 3000;
 connectDB('mongodb://localhost:27017/project_recap');
 const app = express();
 
 app.use('',index_router);
+app.use('/api/students',student_router);
 
 app.listen(port , () => appDebug(`Listening on ${port}....`));
