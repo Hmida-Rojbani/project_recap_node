d617c7d174b439f7e5677a82f6cb0f1493d46902
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-29 15:38:23 +0100

    add post and get

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connectDB = require('./db/connect');
 const port = process.env.PORT || 3000;
 connectDB('mongodb://localhost:27017/project_recap');
 const app = express();
-
+app.use(express.json());
 app.use('',index_router);
 app.use('/api/students',student_router);
 
--- routers/students.js
+++ routers/students.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { Student } = require('../models/student')
+//get All student
+router.get('',async (req,res)=>{
+    var students = await Student.find(); // select * from Student
+    res.send(students);
+});
 
-
+//post new student
+router.post('',async (req,res)=>{
+    var student = new Student({
+        name: req.body.name,
+        age: req.body.age,
+        class : {
+            name : req.body.class.name,
+            max_number : req.body.class.max_number
+        }
+    });
+    let savedStudent = await student.save();
+    res.send(savedStudent);
+});
 
 module.exports = router;
