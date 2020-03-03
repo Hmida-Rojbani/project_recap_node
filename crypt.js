ba1dc684c396137a2f56a45e1c38fde99a943469
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-03-03 10:07:52 +0100

    add users and authentification
    npm i config bcrypt jsonwebtoken lodash

---
+++ crypt.js
@@ -0,0 +1,12 @@
+const bcrypt=require('bcrypt');
+
+//1234-->abcd
+
+async function run(){
+    const salt=await bcrypt.genSalt(10);
+        console.log(salt);
+
+        const hash=await bcrypt.hash('1234',salt);
+        console.log(hash)
+}
+run()
--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const morgan = require('morgan');
 const appDebug = require('debug')('app:debug');
 const index_router = require('./routers/index_router');
 const student_router = require('./routers/students');
+const user_router = require('./routers/users');
+const login_router = require('./routers/auth');
 const connectDB = require('./db/connect');
 const port = process.env.PORT || 3000;
 connectDB('mongodb://localhost:27017/project_recap');
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
+
 app.use('',index_router);
 app.use('/api/students',student_router);
+app.use('/api/users',user_router);
+app.use('/api/login',login_router);
 
 app.listen(port , () => appDebug(`Listening on ${port}....`));
